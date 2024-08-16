7dfed47b360c37716a0a9fc6f27a9c9317db6e33
Davy <42505476+davyranger@users.noreply.github.com>
2024-08-16 16:25:39 +0800

    Delete modules directory

--- modules/cw_age_lin/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lin_ssm_doc_name" {
-  value = aws_ssm_document.install_or_update_cw_agent_lin.name  # Replace aws_resource_name with the actual resource
-}
-
-output "lin_ssm_doc_arn" {
-  value = aws_ssm_document.install_or_update_cw_agent_lin.arn   # Replace aws_resource_name with the actual resource
-}
